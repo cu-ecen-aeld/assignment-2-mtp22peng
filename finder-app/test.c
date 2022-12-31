cb6bd35c411d3c248f77aa411237d337577c6624
Ming-tsan Peng <mt.peng@gmail.com>
2022-12-31 13:24:16 +0800

    ok

---
+++ finder-app/test.c
@@ -0,0 +1,14 @@
+// gcc centro.c -o centro
+
+#include <stdio.h>
+#include <syslog.h>
+
+int  main(int argc, char *argv[])
+{
+    openlog(NULL, 0, LOG_USER);
+
+    syslog(LOG_INFO, "MORTACCI TUA!!!");
+
+    closelog();
+    return 0;
+}
